3864694b004e694b62ac2f25669779eeefd40555
NIU1745924 <1745924@uab.cat>
2026-07-21 18:18:13 +0200

    interacciones basicas añadidas

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const randomButton = document.querySelector("#random-button");
+const resetButton = document.querySelector("#reset-button");
+
+randomButton.addEventListener("click", function () {
+    alert("heeyyy vamos a crear un outfit aleatorio");
+});
+
+resetButton.addEventListener("click", function () {
+    alert("Tu outfit se ha reiniciado jijiji");
+});
